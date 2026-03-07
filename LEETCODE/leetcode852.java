8357765e42905dd273dbaf2ae02d9791fcae38b6
YourName <ag4827550@gmail.com>
2026-03-07 12:58:11 +0530

    update

---
+++ LEETCODE/leetcode852.java
@@ -0,0 +1,26 @@
+package LEETCODE;
+
+class Solution {
+    public int peakIndexInMountainArray(int[] nums) {
+        int n = nums.length;
+        int low = 0;
+        int high = n - 1;
+
+        while (low < high) {
+            int mid = low + (high - low) / 2;
+            if (nums[mid] < nums[mid + 1]) {
+                low = mid + 1;
+            }
+
+            else high = mid;
+
+
+
+        }
+        return low;
+
+    }
+}
+
+public class leetcode852 {
+}
